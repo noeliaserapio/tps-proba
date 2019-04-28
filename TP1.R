95c8f2fb7821269e2441288939bdb4a007330126
GermanDeSousa <gdesousa@eryxsoluciones.com.ar>
2019-04-28 14:33:59 -0300

    Refactor

--- TP1.R
+++ TP1.R
@@ -1,8 +1,16 @@
+### Funciones utiles: 
+album_completo = function(cantidad_de_figuritas) {
+  # Devuelve un album lleno.
+  return(rep(TRUE, cantidad_de_figuritas))
+}
 
-### Definir las siguientes funciones
+album_vacio = function(cantidad_de_figuritas) {
+  # Devuelve un album completo.
+  return(rep(FALSE, cantidad_de_figuritas))
+}
 
 ## Ejercicio 1
-album = rep(FALSE, 6) 
+album = album_vacio(cantidad_de_figuritas=6)
 
 sample = sample(1:6, 1, replace = TRUE)
 
@@ -11,32 +19,33 @@ album[sample] = TRUE
 print(album)
 
 ## Ejercicio 2
-
+# mejor nombre para album_lleno seria es_album_lleno. Devuelve un booleano y no una objeto. Se mantiene así para mantener enunciado
 album_lleno = function(album){
   return(all(album))
 }
 
-mock_album_completo = rep(TRUE, 6)
-mock_album_incompleto = rep(FALSE, 6)
+mock_album_completo = album_completo(cantidad_de_figuritas=6)
+mock_album_incompleto = album_vacio(cantidad_de_figuritas=6)
 
 print(album_lleno(mock_album_completo))
 print(album_lleno(mock_album_incompleto))
 
 ## Ejercicio 3
-
+# Aclaración: se agregó un parámetro más con valor default True (mantiene la usabilidad definida en el enunciado) para 
+# reutilizar código en los siguientes ejercicios. 
 generar_sobre = function(tam_album, tam_sobre, con_repetidos = TRUE){
   return(sample(1:tam_album, tam_sobre, replace=con_repetidos))
 }
 
 sobre = generar_sobre(tam_album=6, tam_sobre=3)
 
-album_vacio = rep(FALSE, 6)
+album_vacio = album_vacio(cantidad_de_figuritas=6)
 
-# Linda forma de hacer TDD! Felicitaciones! 
 album_vacio[sobre[1]] = TRUE
 album_vacio[sobre[2]] = TRUE
 album_vacio[sobre[3]] = TRUE
 
+print(album_vacio)
 
 ## Ejercicio 4
 pegar_sobre = function(album,  sobre){
@@ -46,7 +55,7 @@ pegar_sobre = function(album,  sobre){
   return(album)
 }
 
-album_vacio = rep(FALSE, 10)
+album_vacio = album_vacio(cantidad_de_figuritas=10)
 sobre_1 = generar_sobre(tam_album=length(album_vacio), tam_sobre=3)
 sobre_2 = generar_sobre(tam_album=length(album_vacio), tam_sobre=3)
 
@@ -59,7 +68,7 @@ album = pegar_sobre(album, sobre_2)
 print(album)
 
 ## Ejercicio 5
-cuantas_figuritas = function(tam_album, tam_sobre, sobre_con_repetidos = TRUE){
+cuantas_figuritas = function(tam_album, tam_sobre, sobre_con_repetidos=TRUE){
   cantidad_de_sobres_usados = 0
   album = rep(FALSE, tam_album)
   while(!album_lleno(album=album)) {
@@ -82,11 +91,16 @@ print(tercer_intento)
 print((primer_intento == segundo_intento) & (primer_intento == tercer_intento) & (segundo_intento == tercer_intento))
 
 ## Ejercicio 7
-cantidad_de_repeticiones = 1000
-resultados_de_experimentos = c()
-for(i in 1:cantidad_de_repeticiones) {
-   resultados_de_experimentos = c(resultados_de_experimentos, cuantas_figuritas(tam_album=670, tam_sobre=5))
+generar_experimentos = function(cantidad_de_repeticiones, con_repeticion=TRUE){
+  resultados_de_experimentos = c()
+  for(i in 1:cantidad_de_repeticiones) {
+    resultados_de_experimentos = c(resultados_de_experimentos, cuantas_figuritas(tam_album=670, tam_sobre=5, sobre_con_repetidos=con_repeticion))
+  }  
+  return(resultados_de_experimentos)
 }
+
+cantidad_de_repeticiones = 1000
+resultados_de_experimentos = generar_experimentos(cantidad_de_repeticiones)
 
 plot(1:cantidad_de_repeticiones, resultados_de_experimentos, main = 'Resultados de experimentos')
 
@@ -96,8 +110,8 @@ print(casos_exitosos / cantidad_de_repeticiones)
 
 # La cantidad de sobres que hacen falta comprar para completar el album con probabilidad mayor o igual a 0.9.
 # P(X < x) >= 0.9
-cantidad_sobres_prob_mayor = quantile(resultados_de_experimentos, probs = 0.90)
-print(cantidad_sobres_prob_mayor)
+cantidad_sobres_con_probabilidad_mayor = quantile(resultados_de_experimentos, probs = 0.90)
+print(cantidad_sobres_con_probabilidad_mayor)
 
 # Cual es el valor esperado de sobres para completar el album?
 esperanza = mean(resultados_de_experimentos)
@@ -108,12 +122,8 @@ desviacion = sd(resultados_de_experimentos)
 print(desviacion)
 
 # Ejercicio 8
-
 cantidad_de_repeticiones = 1000
-resultados_de_experimentos = c()
-for(i in 1:cantidad_de_repeticiones) {
-  resultados_de_experimentos = c(resultados_de_experimentos, cuantas_figuritas(tam_album=670, tam_sobre=5, sobre_con_repetidos=FALSE))
-}
+resultados_de_experimentos = generar_experimentos(cantidad_de_repeticiones, con_repeticion=FALSE)
 
 plot(1:cantidad_de_repeticiones, resultados_de_experimentos, main = 'Resultados de experimentos')
 
@@ -123,8 +133,8 @@ print(casos_exitosos / cantidad_de_repeticiones)
 
 # La cantidad de sobres que hacen falta comprar para completar el album con probabilidad mayor o igual a 0.9.
 # P(X < x) >= 0.9
-cantidad_sobres_prob_mayor = quantile(resultados_de_experimentos, probs = 0.90)
-print(cantidad_sobres_prob_mayor)
+cantidad_sobres_con_probabilidad_mayor = quantile(resultados_de_experimentos, probs = 0.90)
+print(cantidad_sobres_con_probabilidad_mayor)
 
 # Cual es el valor esperado de sobres para completar el album?
 esperanza = mean(resultados_de_experimentos)
