c846253b10974931c856bce52c5626fd240f18a4
noeliaserapio <noeliaserapio@users.noreply.github.com>
2019-06-11 14:48:29 -0300

    Update TP2.R
    Hasta ejercicio 5

--- TP2.R
+++ TP2.R
@@ -3,6 +3,11 @@ media_muestral = function(sample){
   return(sum(sample) / length(sample))
 }
 
+# varianza = desvio_estandar^2
+varianza = function(sample){
+  return(sd(sample)^2)
+}
+
 # Ejercicio 1 
 
 # Sea Xi - U(0, b)
@@ -60,10 +65,75 @@ media_muestral_b_momentos = media_muestral(variable_aleatoria_con_b_momentos)
 media_muestral_b_maxima_verosimilitud = media_muestral(variable_aleatoria_con_b_maxima_verosimilitud)
 media_muestral_b_mediana = media_muestral(variable_aleatoria_con_b_mediana)
 
-error_b_momentos = b - media_muestral_b_momentos
-error_b_maxima_verosimilitud = b - media_muestral_b_maxima_verosimilitud
-error_b_mediana = b - media_muestral_b_mediana
+sesgo_b_momentos = b - media_muestral_b_momentos
+sesgo_b_maxima_verosimilitud = b - media_muestral_b_maxima_verosimilitud
+sesgo_b_mediana = b - media_muestral_b_mediana
 
-print(error_b_momentos)
-print(error_b_maxima_verosimilitud)
-print(error_b_mediana)
+print(sesgo_b_momentos)
+print(sesgo_b_maxima_verosimilitud)
+print(sesgo_b_mediana)
+
+# e) 
+varianza_b_momentos = varianza(variable_aleatoria_con_b_momentos)
+varianza_b_maxima_verosimilitud = varianza(variable_aleatoria_con_b_maxima_verosimilitud)
+varianza_b_mediana = varianza(variable_aleatoria_con_b_mediana)
+
+print(varianza_b_momentos)
+print(varianza_b_maxima_verosimilitud)
+print(varianza_b_mediana)
+
+# f) ECM = varianza + sesgo^2
+error_cuadratico_medio = function (varianza, sesgo) {
+  return(varianza + sesgo^2)
+}
+
+ecm_b_momentos = error_cuadratico_medio(varianza_b_momentos, sesgo_b_momentos)
+ecm_b_maxima_verosimilitud = error_cuadratico_medio(varianza_b_maxima_verosimilitud, sesgo_b_maxima_verosimilitud)
+ecm_b_mediana = error_cuadratico_medio(varianza_b_mediana, sesgo_b_mediana)
+
+print(ecm_b_momentos)
+print(ecm_b_maxima_verosimilitud)
+print(ecm_b_mediana)
+
+# Ejercicio 5
+simulacion_mv = function (b, n) {
+  cantidad_de_repeticiones = 1000 
+  variable_aleatoria_con_b_maxima_verosimilitud = c()
+  for(i in 1:cantidad_de_repeticiones){
+  sample = runif(n, min=0, max=b)
+  variable_aleatoria_con_b_maxima_verosimilitud = c(variable_aleatoria_con_b_maxima_verosimilitud, b_maxima_verosimilitud(sample))
+  }
+  media_muestral_b_maxima_verosimilitud = media_muestral(variable_aleatoria_con_b_maxima_verosimilitud)
+  return (list(sesgo_b_maxima_verosimilitud=(b - media_muestral_b_maxima_verosimilitud), varianza_b_maxima_verosimilitud=varianza(variable_aleatoria_con_b_maxima_verosimilitud)))
+}
+
+simulacion_mom = function (b, n) {
+  cantidad_de_repeticiones = 1000 
+  variable_aleatoria_con_b_momentos = c()
+  for(i in 1:cantidad_de_repeticiones){
+    sample = runif(n, min=0, max=b)
+    variable_aleatoria_con_b_momentos = c(variable_aleatoria_con_b_momentos, b_momentos(sample))
+  }
+  media_muestral_b_momentos = media_muestral(variable_aleatoria_con_b_momentos)
+  return (list(sesgo_b_momentos=(b - media_muestral_b_momentos), varianza_b_momentos=varianza(variable_aleatoria_con_b_momentos)))
+}
+
+simulacion_med = function (b, n) {
+  cantidad_de_repeticiones = 1000 
+  variable_aleatoria_con_b_mediana = c()
+  for(i in 1:cantidad_de_repeticiones){
+    sample = runif(n, min=0, max=b)
+    variable_aleatoria_con_b_mediana = c(variable_aleatoria_con_b_mediana, b_mediana(sample))
+  }
+  media_muestral_b_mediana = media_muestral(variable_aleatoria_con_b_mediana)
+  return (list(sesgo_b_mediana=(b - media_muestral_b_mediana), varianza_b_mediana=varianza(variable_aleatoria_con_b_mediana)))
+}
+
+simulacion_mv_b_n = simulacion_mv(1,15)
+print(simulacion_mv_b_n)
+
+simulacion_mom_b_n = simulacion_mom(1,15)
+print(simulacion_mom_b_n)
+
+simulacion_med_b_n = simulacion_med(1,15)
+print(simulacion_med_b_n)
